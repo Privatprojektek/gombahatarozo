db590791f4aae29f3270242d38ff06f4ee1aca3d
csefikatalin <csefi.katalin@gmail.com>
2024-02-07 09:20:20 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate   } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import Admin from "./pages/Admin"
 import Public from "./pages/Public"
 import NoPage from "./pages/NoPage"
@@ -9,17 +10,17 @@ import Layout from "./Layout"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="admin" element={<Admin />} />
         <Route path="public" element={<Public />} />
         <Route path="*" element={<NoPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        
       </Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
